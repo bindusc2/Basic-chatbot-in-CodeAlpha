fba6377bb9699e5acdd69a4079275651d7492d7e
Bindu S C <bindusc2@gmail.com>
2026-08-29 20:14:37 +0530

    Create basic chatbot.py

---
+++ basic chatbot.py
@@ -0,0 +1,27 @@
+print(" Chatbot: Hi, How can I help you?")
+
+while True:
+    if user == "hi":
+        print(" Chatbot: Hello, How are you?")
+
+    elif user == "how are you":
+        print(" Chatbot: I'm fine. How about you?")
+
+    elif user == "i am fine":
+        print(" Chatbot: great to hear")
+
+    elif user == "what is your name":
+        print("Chatbot: I'm a simple Python chatbot")
+
+elif user == "Can you explain python topics in simple language":
+       print("chatbot: you want from basics")
+
+elif user == "yes":
+          print("chatbot: Ok i will prepare")
+
+    elif user == "bye":
+        print(" Chatbot: ok bye,  Have a nice day")
+        
+
+    else:
+        print("Chatbot: invalid type.")
