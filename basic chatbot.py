57497eaa8b482c4fc3b5038e6ed5f872b14b8f34
Bindu S C <bindusc2@gmail.com>
2026-08-29 20:21:45 +0530

    Update basic chatbot.py

--- basic chatbot.py
+++ basic chatbot.py
@@ -1,27 +1,32 @@
-print(" Chatbot: Hi, How can I help you?")
+print("Chatbot: Hi, How can I help you?")
 
 while True:
+    user = input("You: ").lower()
+
     if user == "hi":
-        print(" Chatbot: Hello, How are you?")
+        print("Chatbot: Hello, How are you?")
 
     elif user == "how are you":
-        print(" Chatbot: I'm fine. How about you?")
+        print("Chatbot: I'm fine. How about you?")
 
     elif user == "i am fine":
-        print(" Chatbot: great to hear")
+        print("Chatbot: Great to hear!")
 
     elif user == "what is your name":
-        print("Chatbot: I'm a simple Python chatbot")
+        print("Chatbot: I'm a simple Python chatbot.")
 
-elif user == "Can you explain python topics in simple language":
-       print("chatbot: you want from basics")
+    elif user == "can you explain python topics in simple language":
+        print("Chatbot: Do you want to learn from the basics?")
 
-elif user == "yes":
-          print("chatbot: Ok i will prepare")
+    elif user == "yes":
+        print("Chatbot: Okay, I will explain Python topics in simple language.")
+
+    elif user == "thank you":
+        print("Chatbot: It's my pleasure!")
 
     elif user == "bye":
-        print(" Chatbot: ok bye,  Have a nice day")
-        
+        print("Chatbot: Bye, have a nice day!")
+        break
 
     else:
-        print("Chatbot: invalid type.")
+        print("Chatbot: Invalid input.")
